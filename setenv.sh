463489415863db457b61993826562b86632c6dfa
Gustavo Vázquez Amado <gusjavaz@gmail.com>
2016-11-14 11:56:11 +0100

    Add files via upload

---
+++ setenv.sh
@@ -0,0 +1,7 @@
+export IMAGES=~/carbon-training/images
+export DB=esb
+export CARBON_VERSION=4.4.10
+export ESB_VERSION=4.9.0
+export AS_VERSION=5.3.0
+export SVN_ROOT=~/carbon-training/svn_repositories
+
